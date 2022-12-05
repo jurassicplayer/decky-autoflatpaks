b57215f06cae54a04083c3ac960c831324f7d8f2
jurassicplayer <jurassicplayer75@gmail.com>
2022-12-05 02:49:49 -0800

    Created encompassing flatpak metadata

--- src/Utils/Flatpak.d.ts
+++ src/Utils/Flatpak.d.ts
@@ -1,3 +1,33 @@
+export interface FlatpakMetadata {
+  name:             string
+  application:      string
+  description:      string
+  version:          string
+  branch:           string
+  arch:             string
+  origin:           string
+  ref:              string
+  runtime:          string
+  options:          string
+  installed_size:   string
+  // remote only
+  commit?:           string
+  download_size?:    string
+  // local only
+  active?:          string
+  installation?:    string
+  latest?:          string
+  // update only
+  partial?:         boolean
+  // plugin computed metadata
+  packagetype:      string
+  updateable:       boolean
+  masked:           boolean
+  installed:        boolean
+  parent?:          string
+  parentMasked?:    boolean
+}
+
 export interface FlatpakUpdate {
   application:    string
   branch:         string
@@ -8,35 +38,42 @@ export interface FlatpakUpdate {
 }
 
 export interface LocalFlatpakMetadata {
-  name: string,
-  application: string,
-  description: string,
-  version: string,
-  branch: string,
-  arch: string,
-  origin: string,
-  ref: string,
-  runtime: string,
-  options: string,
-  active?: string,
-  installation?: string,
-  latest?: string,
-  size?: string,
+  name: string
+  application: string
+  description: string
+  version: string
+  branch: string
+  arch: string
+  origin: string
+  ref: string
+  runtime: string
+  options: string
+  installed_size: string
+  // Local flatpak specific
+  active: string
+  installation: string
+  latest: string
+  // Plugin specific
   packagetype: string
+  parent?: string
 }
 
 export interface RemoteFlatpakMetadata {
-  name: string,
-  application: string,
-  description: string,
-  version: string,
-  branch: string,
-  arch: string,
-  origin: string,
-  ref: string,
-  runtime: string,
-  options: string,
-  commit?: string,
-  installed_size?: string,
-  download_size?: string,
+  name: string
+  application: string
+  description: string
+  version: string
+  branch: string
+  arch: string
+  origin: string
+  ref: string
+  runtime: string
+  options: string
+  installed_size: string
+  // Remote flatpak specific
+  commit: string
+  download_size: string
+  // Plugin specific
+  packagetype: string
+  parent?: string
 }
