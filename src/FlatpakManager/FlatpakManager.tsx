c188168904f8e0d152a948f9a1b4c0df21ed5cb8
jurassicplayer <jurassicplayer75@gmail.com>
2022-12-05 02:55:06 -0800

    Small changes to tab router

--- src/FlatpakManager/FlatpakManager.tsx
+++ src/FlatpakManager/FlatpakManager.tsx
@@ -1,23 +1,22 @@
-import { Tabs } from "decky-frontend-lib";
-import { VFC, useEffect, useState } from "react";
-import { BrowsePage } from "./BrowsePage"
-import { InstalledPage } from "./InstalledPage"
-
+import { Focusable, Tabs } from "decky-frontend-lib"
+import { VFC, useEffect, useState } from "react"
+import { BrowsePage } from "./Browse/BrowsePage"
 
 export const FlatpakManager: VFC = () => {
-  const [currentTabRoute, setCurrentTabRoute] = useState<string>("installed")
-
+  const [currentTabRoute, setCurrentTabRoute] = useState<string>("browser")
+  
   useEffect(() => {
     console.log("On component loaded")
   },[])
+
   return (
+    <Focusable style={{minWidth: "100%", minHeight: "100%"}}>
     <div
       style={{
         marginTop: "40px",
         height: "calc(100% - 40px)",
-        background: "#0c1519",
-      }}
-    >
+        background: "#0c1519"
+      }}>
       <Tabs
         activeTab={currentTabRoute}
         // @ts-ignore
@@ -29,14 +28,10 @@ export const FlatpakManager: VFC = () => {
             title: "Browse",
             content: <BrowsePage />,
             id: "browser",
-          },
-          {
-            title: "Installed",
-            content: <InstalledPage />,
-            id: "installed",
-          },
+          }
         ]}
       />
     </div>
-  );
-};
+    </Focusable>
+  )
+}
