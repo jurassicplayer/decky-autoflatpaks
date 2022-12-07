4fce724bcfb353b478f69ec363afe60f25ac6407
jurassicplayer <jurassicplayer75@gmail.com>
2022-12-07 13:20:59 -0800

    Add basic logging functionality

--- src/FlatpakManager/Logger/LoggerPage.tsx
+++ src/FlatpakManager/Logger/LoggerPage.tsx
@@ -1,8 +1,22 @@
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react"
+import { Focusable } from "decky-frontend-lib"
+import { Backend } from "../../Utils/Backend"
+import { JournalEntry } from "../../Utils/History"
 
 export const LoggerPage: VFC = () => {
+  const [history, setHistory] = useState<JournalEntry[]>([])
+  useEffect(() => {
+    Backend.getPackageHistory().then((history) => setHistory(history))
+  }, [])
+  useEffect(() => {
+    console.log(history)
+  }, [history])
+
   return (
-    <div>
+    <Focusable>
+      {history ? 
+        history.map(entry => {return (<div>{entry.MESSAGE}</div>)})
+      : null}
       <h2>Work In Progress</h2>
       <p>
         This is a tentative area that will be a place that acts as a UI to the "flatpak history" command.
@@ -11,6 +25,6 @@ export const LoggerPage: VFC = () => {
         <li>Provides options for since time, until time, reverse order</li>
         <li>Lists installs, updates, and removals of applications and runtimes</li>
       </ul>
-    </div>
+    </Focusable>
   )
 }
