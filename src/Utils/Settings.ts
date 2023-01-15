7f3d9e9103fd8e5c900eb69e2cfa59084d2c47d2
jurassicplayer <jurassicplayer75@gmail.com>
2023-01-15 06:34:01 -0800

    Add debug message for loaded settings

--- src/Utils/Settings.ts
+++ src/Utils/Settings.ts
@@ -10,17 +10,20 @@ export class Settings {
 
   static async loadFromLocalStorage() {
     var returncode = true
+    let settings = '[AutoFlatpaks] Loaded settings from storage'
     for (let key in this) {
       try {
         if (typeof this[key] == "boolean") this[key] = (await Backend.getSetting(key, this[key])) as boolean
         else if (typeof this[key] == "number") this[key] = (await Backend.getSetting(key, this[key])) as number
         else if (typeof this[key] == "string") this[key] = (await Backend.getSetting(key, this[key])) as string
         else if (this[key] instanceof Date) this[key] = new Date((await Backend.getSetting(key, this[key])).toString())
+        settings += `\n\t${key}: ${this[key]}`
       } catch (error) {
         returncode = false
         console.debug('[AutoFlatpaks] Failed to load setting: ', key)
       }
     }
+    console.debug(settings)
     return returncode
   }
 
