d49c6caa4b3740d28b143ae72b0a68b3425f6a34
jurassicplayer <jurassicplayer75@gmail.com>
2023-01-18 03:30:45 -0800

    Fix updateable button being shown for all statuses

--- src/QAM/StatusBar.tsx
+++ src/QAM/StatusBar.tsx
@@ -6,6 +6,10 @@ import { events } from "../Utils/Events"
 import { UpdateablePackagesModal } from "./UpdateablePackages"
 
 export const StatusBar = () => {
+  const onAppStateChange = (e: Event) => {
+    let event = e as events.AppStateEvent
+    setAppState(event.appState)
+  }
   const onUpdateList = (e: Event) => {
     let event = e as events.UpdateListEvent
     setUpdateList(event.updateList)
@@ -25,8 +29,20 @@ export const StatusBar = () => {
       queueLength: undefined
     })
   }
-  const onAppStateChange = (e: Event) => { setAppState((e as events.AppStateEvent).appState) }
 
+  const [updateList, setUpdateList] = useState<string[]>(Backend.getUpdateList())
+  const [appState, setAppState] = useState<number>(Backend.getAppState())
+  const [ShowUpdateList, setShowUpdateList] = useState<boolean>(appState == appStates.idle && updateList.length > 0)
+  const [hover, setHover] = useState<boolean>(false)
+  const [queueProgress, setQueueProgress] = useState<{[key: string]: any}>({
+    currentItem: Backend.getQueue()[0],
+    queueProgress: Backend.getQueueProgress(),
+    queueLength: Backend.getQueueLength()
+  })
+
+  useEffect(() => {
+    setShowUpdateList(appState == appStates.idle && updateList.length > 0)
+  }, [appState, updateList])
   useEffect(() => {
     // Register listener
     Backend.eventBus.addEventListener(events.UpdateListEvent.eType, onUpdateList)
@@ -41,18 +57,8 @@ export const StatusBar = () => {
     Backend.eventBus.removeEventListener(events.AppStateEvent.eType, onAppStateChange)
   }, [])
 
-  const [updateList, setUpdateList] = useState<string[]>(Backend.getUpdateList())
-  const [hover, setHover] = useState<boolean>(false)
-  const [appState, setAppState] = useState<number>(Backend.getAppState())
-  const [queueProgress, setQueueProgress] = useState<{[key: string]: any}>({
-    currentItem: Backend.getQueue()[0],
-    queueProgress: Backend.getQueueProgress(),
-    queueLength: Backend.getQueueLength()
-  })
-
   let StatusText = ""
   let CSS = StatusBarCSS.Default
-  let ShowUpdateList = (appState == appStates.idle && updateList.length > 0)
   if (ShowUpdateList) {
     StatusText = `${updateList.length} update${updateList.length > 1 ? 's' : ''} available`
     CSS = StatusBarCSS.CheckForUpdates
@@ -73,17 +79,19 @@ export const StatusBar = () => {
   }
   return (
     <PanelSectionRow>
-      { appState != appStates.idle || (ShowUpdateList)
+      { appState != appStates.idle || ShowUpdateList
       ? <div style={CSS}>
-          <DialogButton
-            onGamepadFocus={() => setHover(true)}
-            onGamepadBlur={() => setHover(false)}
-            onOKActionDescription="Updates"
-            onClick={() => {showModal(<UpdateablePackagesModal/>, findSP(), {popupHeight: 100})}}
-            style={hover ? StatusBarCSS.HiddenButtonHover : StatusBarCSS.HiddenButton }
-            disabled={!ShowUpdateList}>
-            {StatusText}
-          </DialogButton>
+          { ShowUpdateList 
+          ? <DialogButton
+              onGamepadFocus={() => setHover(true)}
+              onGamepadBlur={() => setHover(false)}
+              onOKActionDescription="Updates"
+              onClick={() => {showModal(<UpdateablePackagesModal/>, findSP(), {popupHeight: 100})}}
+              style={hover ? StatusBarCSS.HiddenButtonHover : StatusBarCSS.HiddenButton }
+              disabled={!ShowUpdateList}>
+              {StatusText}
+            </DialogButton>
+          : <div>{StatusText}</div> }
         </div>
       : null }
     </PanelSectionRow>
