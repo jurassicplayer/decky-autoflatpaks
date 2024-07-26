157cab40fa04855001bc0409321a22d8ce7a5ebb
jurassicplayer <jurassicplayer75@gmail.com>
2024-07-26 00:57:25 -0700

    Clone new plugin template main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+
+# The decky plugin module is located at decky-loader/plugin
+# For easy intellisense checkout the decky-loader code repo
+# and add the `decky-loader/plugin/imports` path to `python.analysis.extraPaths` in `.vscode/settings.json`
+import decky
+import asyncio
+
+class Plugin:
+    # A normal method. It can be called from the TypeScript side using @decky/api.
+    async def add(self, left: int, right: int) -> int:
+        return left + right
+
+    async def long_running(self):
+        await asyncio.sleep(15)
+        await decky.emit("test_event", "Hello from the backend!", True, 2)
+
+    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+    async def _main(self):
+        self.loop = asyncio.get_event_loop()
+        decky.logger.info("Hello World!")
+
+    # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
+    # completely removed
+    async def _unload(self):
+        decky.logger.info("Goodnight World!")
+        pass
+
+    # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
+    # plugin that may remain on the system
+    async def _uninstall(self):
+        decky.logger.info("Goodbye World!")
+        pass
+
+    async def start_timer(self):
+        self.loop.create_task(self.long_running())
+
+    # Migrations that should be performed before entering `_main()`.
+    async def _migration(self):
+        decky.logger.info("Migrating")
+        # Here's a migration example for logs:
+        # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
+        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
+                                               ".config", "decky-template", "template.log"))
+        # Here's a migration example for settings:
+        # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
+        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
+        decky.migrate_settings(
+            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
+            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
+        # Here's a migration example for runtime data:
+        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
+        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
+        decky.migrate_runtime(
+            os.path.join(decky.DECKY_HOME, "template"),
+            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
