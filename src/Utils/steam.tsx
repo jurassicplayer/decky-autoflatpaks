c0677dfa422497af243253fe16966019c4ab2d6b
jurassicplayer <jurassicplayer75@gmail.com>
2025-06-13 23:09:16 -0700

    Revert "git tracked files screwed up"
    This reverts commit cd02d951dc1b8ca40c3152ebf4cbaf844fd20df3.

--- src/Utils/steam.tsx
+++
@@ -1,5 +0,0 @@
-import { SVGProps } from "react"
-
-export const SteamChevronDown = (props:SVGProps<SVGSVGElement>) => {
-  return <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" fill="none" direction="down"><path fill="currentColor" d="M31 20.3606L18.0204 33L5 20.3606L8.60376 16.8568L18.0204 25.9924L27.4166 16.8568L31 20.3606ZM27.3962 3L18.0204 12.1356L8.62412 3L5 6.50379L18.0204 19.1432L31 6.50379L27.3962 3Z"></path></svg>
-}
